ec042bfd21ffbcc07a54ffcc9f40186f9ee74e8f
dfranciszczak <dfranciszczak@outlook.com>
2018-11-30 16:22:16 +0100

    added Projects view

--- server.js
+++ server.js
@@ -50,6 +50,12 @@ app.get('/bad', (req, res) => {
   });
 });
 
+app.get('projects', (res, req) => {
+  res.render('project.hbs', {
+    pageTitle: 'Projects'
+  });
+})
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
